dc58a7a4112b2d8a2788ca22e9ddf71cbb607801
Irina Abdriaeva <irina.abdrayeva@gmail.com>
2017-02-12 15:30:38 +1100

    Refactoring of navigation

--- test/pageObjects/SignInPage.js
+++ test/pageObjects/SignInPage.js
@@ -1,7 +1,15 @@
 const BasePage = require('./BasePage');
+const InvoicesPage = require('./InvoicesPage');
+
 class SignInPage extends BasePage {
     navigate() {
         browser.url('/users/sign_in');
+    }
+    sginIn(login, password) {
+        browser.element('.container #user_email').setValue(login);
+        browser.element('.container #user_password').setValue(password);
+        browser.element('.container input[value="Sign in"]').click();
+        return new InvoicesPage();
     }
 }
 
--- test/specs/sign-in.spec.js
+++ test/specs/sign-in.spec.js
@@ -1,21 +1,31 @@
-const BasicNavigation = require('../pageObjects/BasicNavigation');
+const AnonNavigation = require('../pageObjects/AnonNavigation');
 
 describe('sign in page', ()=> {
 
+    afterEach(() => {
+        browser.deleteCookie();
+    });
+
     it('should navigate to sign in page', () => {
-        BasicNavigation.navigateToSignIn().getHeaderText().should.be.equal('Sign in');
+        AnonNavigation.navigateToSignIn().getHeaderText().should.be.equal('Sign in');
     });
 
     it('should redirect to sign in page from invoices if not logged in', () => {
-        BasicNavigation.navigateToInvoices().getHeaderText().should.be.equal('Sign in');
+        AnonNavigation.navigateToInvoices().getHeaderText().should.be.equal('Sign in');
     });
 
     it('should redirect to sign in page from clients if not logged in', () => {
-        BasicNavigation.navigateToClients().getHeaderText().should.be.equal('Sign in');
+        AnonNavigation.navigateToClients().getHeaderText().should.be.equal('Sign in');
     });
 
     it('should redirect to sign in page from invoice page if not logged in', () => {
-        BasicNavigation.navigateToInvoice(7).getHeaderText().should.be.equal('Sign in');
+        AnonNavigation.navigateToInvoice(7).getHeaderText().should.be.equal('Sign in');
     });
 
+    it('should redirect to invoices  after sign in', () => {
+        AnonNavigation
+            .navigateToSignIn()
+            .sginIn('IrinaAbdriaevaMailForTests@gmail.com', 'JNaO8NBlyOZrepf14tgG')
+            .getHeaderText().should.be.equal('Invoices');
+    });
 });
